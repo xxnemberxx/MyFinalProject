1cf51e08213d03126e10a7b9e35a904fa87b745a
Yunus Emre Kalaycı <47313044+xxnemberxx@users.noreply.github.com>
2021-07-31 22:40:09 +0300

    Add files via upload

---
+++ NorthwindCS.sql
@@ -0,0 +1,21 @@
+﻿-- Select -- 
+SELECT ContactName Adi, CompanyName SirketAdi,City Sehir FROM Customers
+
+SELECT * FROM Products WHERE CategoryID = 1 and UnitPrice > 10
+
+-- ORDER BY Sıralama yapar ASC küçükten büyüğe, DESC büyükten küçüğe
+-- ASC veya DESC yazılmadığı taktirde, varsayılan olarak (ASC) küçükten büyüğe sıralar
+SELECT * FROM Products ORDER BY ProductName 
+
+
+-- GROUP BY  -> COUNT(), MAX(), MIN(), SUM(), AVG()
+
+-- Tablonun satır sayısını döndürür COUNT()
+SELECT COUNT(*) Adet FROM Products 
+
+SELECT COUNT(*) Adet FROM Products WHERE UnitsInStock > 15
+
+-- Tablodaki tekrar eden kategorileri gruplayıp, hangi kategoride ne kadar ürün olduğunu yazdıran sorgu
+-- Group by sorgusu Having' dir.
+select CategoryID, count(*) Unit from Products group by CategoryID having  count(*) < 10
+
